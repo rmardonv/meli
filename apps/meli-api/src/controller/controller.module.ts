4382865a39c53cca0a8d6c8fca80075a2e74f808
Roman Mardones <ext_rpmardonesv@falabella.cl>
2022-04-26 12:17:56 -0400

    remove duplicate  controller

--- apps/meli-api/src/controller/controller.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CoreModule } from '../app/infrastructure/core/core.module';
-
-@Module({
-    imports: [CoreModule]
-})
-export class ControllerModule { }
